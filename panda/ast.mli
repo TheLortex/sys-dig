ad6316c25648da47454b3e4c6fc24967e84bdbfb
Malachi Voss <mvoss@clipper.ens.fr>
2017-01-11 18:22:12 +0100

    Replace ast.mli

--- panda/ast.mli
+++ panda/ast.mli
@@ -1,20 +1,29 @@
+	(* AST for bamboo *)
+	
+	(* Comments are of form &...........\n *)
+	 
+	(* DO NOT:
+		-> do aliases inside conditional structures
+		-> stick anything besides registers/direct variables in conditional tests
+		-> call ANYTHING main*)
 type ident = string
 type register = char
 type binop = Add|Sub|Mul|Div|Rem|And|Or|Xor|Nand|Eq
 type expr =
-	|Var of ident
+	|Val of ident * expr
+	|Pointer of ident
 	|Imm of int
 	|Reg of register
 	|Binop of expr * binop * expr
 	|Not of expr
-type instr =
-	|Label of ident
-	|Goto of ident
-	|Rename of register * ident
-	|Set of register * expr
-	|Store of register * expr
-	|Load of register * expr
-	|Locif of expr * (instr list) * (instr list)
-	|If of expr * (instr list) * (instr list)
-	|While of expr * (instr list)
-type program = instr list
+type 'a instr =
+	|Label of ident * 'a
+	|Goto of ident * 'a
+	|Rename of register * ident * 'a
+	|Set of register * expr * 'a
+	|Store of register * expr * 'a
+	|Load of register * expr * 'a
+	|Locif of expr * ('a instr list) * ('a instr list) * 'a
+	|If of expr * ('a instr list) * ('a instr list) * 'a
+	|While of expr * ('a instr list) * 'a
+type 'a program = 'a instr list
