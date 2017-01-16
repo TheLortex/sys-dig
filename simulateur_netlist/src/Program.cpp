e78fde59501ea3e54d96e24c0484fd9257ef75a7
Lortex <lucas.pluvinage@gmail.com>
2017-01-16 03:57:34 +0100

    fixed bugs

--- simulateur_netlist/src/Program.cpp
+++ simulateur_netlist/src/Program.cpp
@@ -312,9 +312,9 @@ void Program::write_display_output(std::ofstream &cppfile)
         cppfile << ") << std::endl;\n";
     }
 
+    cppfile << "if(x.stopped()) {t.join();return 0;}\n";
     for(Var* v : _display_vars) {
       cppfile << "x.update(\"" << v->get_name() << "\"," << v->get_cpp_name() << ");\n";
     }
-
     cppfile << "\n";
 }
