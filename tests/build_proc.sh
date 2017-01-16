07bb9d3336964ba6daeceb447024123121fa015f
nappy <mathieu.fehr@ens.fr>
2017-01-16 06:56:28 +0100

    changed build_proc

--- tests/build_proc.sh
+++ tests/build_proc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cpp -P ../mjblocks/proco.mmj > proc.mj
 ../minijazz/mjc.native proc.mj
-../simulateur_netlist/compilateur_netlist proc.net proc.out "|sec0||sec1| |min0||min1| |hour0||hour1|\n|day0||day1| |month0||month1|\n|year0||year1||year2||year3|"
+../simulateur_netlist/compilateur_netlist proc.net proc.out "|hour0||hour1| |min0||min1| |sec0||sec1| \n|day0||day1| |month0||month1|\n|year0||year1||year2||year3|"
